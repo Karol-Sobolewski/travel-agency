e14162884618c7b79da22b1a4d66f4af56f93db6
Karol <karolsobolewski92@gmail.com>
2020-06-26 22:57:38 +0200

    Fix fitering trips for selected country && Fix filtering country for selected trip

--- src/components/views/Trips/TripsContainer.js
+++ src/components/views/Trips/TripsContainer.js
@@ -2,8 +2,11 @@ import {connect} from 'react-redux';
 import Trips from './Trips';
 import {getFilteredTrips} from '../../../redux/tripsRedux';
 
-const mapStateToProps = state => ({
-  trips: getFilteredTrips(state),
-});
+const mapStateToProps = (state, props) => {
+  const id = props.match.params.id;
+  return{
+    trips: getFilteredTrips(state, id),
+  };
+};
 
 export default connect(mapStateToProps)(Trips);
