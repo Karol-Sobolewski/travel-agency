e09425752d356ca8800b3c26220587b4ae070ec2
Karol <karolsobolewski92@gmail.com>
2020-07-02 17:15:36 +0200

    Add trip filtering by duration and tags

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -12,22 +12,18 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
-
+  output = output.filter(trip => trip.days >= filters.duration.from && trip.days <= filters.duration.to);
   // TODO - filter by tags
-
+  if(filters.tags){
+    const pattern = new RegExp(filters.tags, 'i');
+    output = output.filter(trip => pattern.test(trip.tags));
+  }
   // TODO - sort by cost descending (most expensive goes first)
+
 
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
-  const filtered = trips.filter(trip => trip.id == tripId);
-
-  // TODO - filter trips by tripId
-
-  console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
-};
 
 export const getTripsForCountry = ({trips}, countryCode) => {
   const filtered = trips.filter(trip => trip.country.code == countryCode);
